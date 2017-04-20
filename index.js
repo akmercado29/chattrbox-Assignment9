1e8a837620dca3bd9a211d0ef769e97e139a62cb
Arjen Mercado <arjen@Arjens-MacBook-Pro.local>
2017-04-19 20:46:44 -0700

    beautified

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ var extract = require('./extract');
 var mime = require('mime');
 var wss = require('./websockets-server');
 
-var handleError = function (err, res) {
+var handleError = function(err, res) {
     res.writeHead(404);
     res.end();
 }
 
-var server = http.createServer(function (req, res) {
+var server = http.createServer(function(req, res) {
     console.log('Resonding to a request.');
 
     var filePath = extract(req.url);
     var fileType = mime.lookup(filePath);
-    fs.readFile(filePath, function (err, data) {
+    fs.readFile(filePath, function(err, data) {
         if (err) {
             handleError(err, res);
             return;
